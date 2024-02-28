bac40d4da11995aca644b0ba059446907eac9b37
UnizaMatBre <breznican1@stud.uniza.sk>
2024-02-28 12:51:25 +0100

    Initial commit

---
+++ src/main/java/user/matbre/App.java
@@ -0,0 +1,13 @@
+package user.matbre;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
